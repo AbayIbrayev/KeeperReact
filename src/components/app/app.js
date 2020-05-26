6b2f267c37f640bd3f126f9f99dbba05e373e51e
Abay <abayibrayev7@gmail.com>
2020-05-26 06:36:42 +0100

    replaced the create notes function with an arrow function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,15 +4,15 @@ import Footer from "../footer";
 import Note from "../note";
 import notes from "../../notes";
 
-function createNotes({ key, title, content }) {
-  return <Note key={key} title={title} content={content} />;
-}
-
 const app = () => {
   return (
     <>
       <Header />
-      <main>{notes.map(createNotes)}</main>
+      <main>
+        {notes.map(({ key, title, content }) => {
+          return <Note key={key} title={title} content={content} />;
+        })}
+      </main>
       <Footer />
     </>
   );
